6bcb9b7898153b50ee3be31c5be6351c05d67f00
jeeyoon <jeeyoon744@naver.com>
2022-03-09 12:51:11 +0900

    [ADD] 16.5 Personalized ~

---
+++ 16.5_Personlized_Ranking_loss.py
@@ -0,0 +1,22 @@
+import torch
+import torch.nn as nn
+
+class HingeLossbRec(nn.Module):
+    def __init__(self, weight=None, batch_axis=0, **kwargs):
+        super(HingeLossbRec, self).__init__(weight=None, batch_axis=0,
+                                            **kwargs)
+
+    def forward(self, positive, negative, m
+    argin=1):
+        loss = nn.MultiMarginLoss()
+        return loss(positive, negative)
+
+class BPRLoss(nn.Module):
+    def __init__(self, **kwargs):
+        super(BPRLoss, self).__init__(**kwargs)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, positive, negative):
+        distances = positive - negative
+        loss = - torch.sum(torch.log(self.sigmoid(distances)))
+        return loss
